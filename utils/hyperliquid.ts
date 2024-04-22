631dfe16cfd54966f962f0fcc5563b4d1d7f7c79
Dennoh Peter <mail@dennohpeter.com>
2024-04-22 11:50:16 +0300

    upgrade hyperliquid integration

--- utils/hyperliquid.ts
+++ utils/hyperliquid.ts
@@ -1,4 +1,4 @@
-import { Wallet, providers, utils, constants } from 'ethers';
+import { providers, utils, constants } from 'ethers';
 import {
   Cancel,
   CandleSnapshot,
@@ -8,10 +8,9 @@ import {
   SubAccount,
 } from '@/types/hyperliquid';
 import { timestamp } from './timestamp';
-import { signInner, signL1Action } from './signing';
+import { signInner } from './signing';
 
 export class Hyperliquid {
-  // ----------------- PRIVATE -----------------
   private chain: Chain;
   private base_url: string;
 
@@ -26,9 +25,8 @@ export class Hyperliquid {
     request: Record<string, any>
   ): Promise<{
     success: boolean;
-    data: Record<string, any> | SubAccount[] | null;
+    data: Record<string, any> | SubAccount[] | null | string;
     msg: String | null;
-    error_type: String | null;
   }> => {
     return fetch(this.base_url, {
       method: 'POST',
@@ -36,13 +34,13 @@ export class Hyperliquid {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(request),
+      credentials: 'include',
     }).then((res) => res.json());
   };
 
   // ----------------- EXCHANGE => PLACE ORDER <= -----------------
 
   placeOrder = async (
-    signer: Wallet,
     asset: number,
     isBuy: boolean,
     price: number | string,
@@ -52,10 +50,6 @@ export class Hyperliquid {
     cloid: string | null = null,
     vaultAdress: string | null = null
   ) => {
-    // FIXME: functionality not tested
-
-    let nonce = timestamp();
-
     // TODO: parse price and quantity
 
     let action = {
@@ -73,20 +67,9 @@ export class Hyperliquid {
       type: 'order',
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
       action,
-      isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
@@ -94,12 +77,9 @@ export class Hyperliquid {
   };
 
   cancelOrder = async (
-    signer: Wallet,
     cancels: Cancel[],
     vaultAdress: string | null = null
   ) => {
-    let nonce = timestamp();
-
     let action = {
       type: 'cancel',
       cancels: cancels.map((cancel) => ({
@@ -108,60 +88,36 @@ export class Hyperliquid {
       })),
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
       action,
-      isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
     return this.#post(request);
   };
 
-  normalTpSl = async (signer: Wallet, asset: number) => {
+  normalTpSl = async (asset: number) => {
     // TODO: Implement normalTpSl
   };
 
   updateLeverage = async (
-    signer: Wallet,
     asset: number,
     isCross: boolean,
     leverage: number,
     vaultAdress: string | null = null
   ) => {
-    let nonce = timestamp();
-
     let action = {
-      type: 'updateLeverage',
       asset,
       isCross,
       leverage,
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
+      type: 'updateLeverage',
       action,
       isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
@@ -169,35 +125,21 @@ export class Hyperliquid {
   };
 
   updateIsolatedMargin = async (
-    signer: Wallet,
     asset: number,
     isBuy: boolean,
     ntli: number,
     vaultAdress: string | null = null
   ) => {
-    let nonce = timestamp();
-
     let action = {
-      type: 'updateIsolatedMargin',
       asset,
       isBuy,
       ntli: utils.parseUnits(ntli.toString(), 6).toNumber(),
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
+      type: 'updateIsolatedMargin',
       action,
-      isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
@@ -206,7 +148,6 @@ export class Hyperliquid {
 
   // ----------------- EXCHANGE => TWAP <= -----------------
   placeTwapOrder = async (
-    signer: Wallet,
     asset: number,
     isBuy: boolean,
     minutes: number,
@@ -215,11 +156,7 @@ export class Hyperliquid {
     randomize: boolean,
     vaultAdress: string | null = null
   ) => {
-    // FIXME: functionality not tested
-    let nonce = timestamp();
-
     let action = {
-      type: 'twapOrder',
       a: asset,
       b: isBuy,
       m: minutes,
@@ -228,20 +165,10 @@ export class Hyperliquid {
       t: randomize,
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
+      type: 'twapOrder',
       action,
-      isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
@@ -250,38 +177,23 @@ export class Hyperliquid {
 
   // ----------------- EXCHANGE => SCALE ORDER <= -----------------
   placeScaleOrder = async (
-    signer: Wallet,
     asset: number,
     isBuy: boolean,
     quantity: number | string,
     scale: number,
     vaultAdress: string | null = null
   ) => {
-    // FIXME: functionality not tested
-    let nonce = timestamp();
-
     let orders = [{ a: asset, b: isBuy, s: quantity.toString(), c: scale }];
 
     let action = {
       grouping: 'na',
-      type: 'order',
       orders,
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
+      type: 'order',
       action,
-      isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
@@ -290,32 +202,15 @@ export class Hyperliquid {
 
   // ----------------- EXCHANGE => SUB ACCOUNTS <= -----------------
 
-  createSubAccount = async (
-    signer: Wallet,
-    name: String,
-    vaultAdress = null
-  ) => {
-    let nonce = timestamp();
-
+  createSubAccount = async (name: String, vaultAdress = null) => {
     let action = {
-      type: 'createSubAccount',
       name,
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
+      type: 'createSubAccount',
       action,
-      isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
@@ -323,33 +218,19 @@ export class Hyperliquid {
   };
 
   subAccountModify = async (
-    signer: Wallet,
     name: String,
     subAccountUser: String,
     vaultAdress = null
   ) => {
-    let nonce = timestamp();
-
     let action = {
-      type: 'subAccountModify',
       subAccountUser,
       name,
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
+      type: 'subAccountModify',
       action,
-      isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
@@ -357,44 +238,37 @@ export class Hyperliquid {
   };
 
   subAccountTransfer = async (
-    signer: Wallet,
     isDeposit: boolean,
     subAccountUser: String,
     usd: number | string,
     vaultAdress = null
   ) => {
-    let nonce = timestamp();
-
     let action = {
-      type: 'subAccountTransfer',
       subAccountUser,
       isDeposit,
       usd: utils.parseUnits(usd.toString(), 6).toNumber(),
     };
 
-    let signature = await signL1Action(
-      signer,
-      action,
-      nonce,
-      this.chain,
-      vaultAdress
-    );
-
     let request = {
       endpoint: 'exchange',
+      type: 'subAccountTransfer',
       action,
-      isFrontend: true,
-      nonce,
-      signature,
       vaultAdress,
     };
 
     return this.#post(request);
   };
 
-  connect = async (
+  connect = async (user: String) => {
+    return this.#post({
+      endpoint: 'connect',
+      user,
+    });
+  };
+
+  connectAgent = async (
     signer: providers.JsonRpcSigner,
-    agent: Wallet,
+    agentAddress: String,
     extra_agent_name: String | null = null,
     vaultAdress = null
   ) => {
@@ -404,9 +278,9 @@ export class Hyperliquid {
       extra_agent_name
         ? utils.defaultAbiCoder.encode(
             ['address', 'string'],
-            [agent.address, extra_agent_name]
+            [agentAddress, extra_agent_name]
           )
-        : utils.defaultAbiCoder.encode(['address'], [agent.address])
+        : utils.defaultAbiCoder.encode(['address'], [agentAddress])
     );
 
     let action = {
@@ -414,7 +288,7 @@ export class Hyperliquid {
         connectionId,
         source: 'https://hyperliquid.xyz',
       },
-      agentAddress: agent.address,
+      agentAddress,
       chain: this.chain,
       type: 'connect',
     };
@@ -442,8 +316,8 @@ export class Hyperliquid {
 
     let request = {
       endpoint: 'exchange',
+      type: 'connect',
       action,
-      isFrontend: true,
       nonce,
       signature,
       vaultAdress,
