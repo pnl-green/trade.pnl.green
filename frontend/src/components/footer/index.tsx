f50e591b7e4ed72d2df43cc8b19bf88f66c67283
Max Fineskin <dev.fineskin@gmail.com>
2024-09-23 15:11:35 +0300

    Feat: updated styles

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { FooterContainer } from '@/styles/footer.styles';
 const Footer = () => {
   return (
     <FooterContainer>
-      <h1>The Chart interface is provided by <a href="tradingview.com" target="_blank">Tradingview</a> - Keep track the most important cryptos like bitcoin usd, ethusd and much more.</h1>
+      The Chart interface is provided by <a href="tradingview.com" target="_blank"> Tradingview </a> - Keep track the most important cryptos like bitcoin usd, ethusd and much more.
     </FooterContainer>
   );
 };
--- frontend/src/styles/footer.styles.ts
+++ frontend/src/styles/footer.styles.ts
@@ -6,14 +6,12 @@ export const FooterContainer = styled(Box)(() => ({
   alignItems: 'center',
   padding: '20px 30px',
   backgroundColor: '#000',
-  textAlign: 'center',
   border: '2px solid rgba(255, 255, 255, 0.1)',
-  h1: {
-    textAlign: 'center',
-    fontSize: '13px',
-    fontWeight: '300',
-    a: {
-      textDecoration: 'none',
-    }
+  textAlign: 'center',
+  fontSize: '13px',
+  fontWeight: '300',
+  a: {
+    padding: '0 5px',
+    textDecoration: 'none',
   }
 }));
