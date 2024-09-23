da06111aface2f54c3b86adb894417ccf291dc63
Max Fineskin <dev.fineskin@gmail.com>
2024-09-23 15:00:44 +0300

    Feat: added footer

---
+++ frontend/src/styles/footer.styles.ts
@@ -0,0 +1,19 @@
+import { Box, styled } from '@mui/material';
+
+export const FooterContainer = styled(Box)(() => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '20px 30px',
+  backgroundColor: '#000',
+  textAlign: 'center',
+  border: '2px solid rgba(255, 255, 255, 0.1)',
+  h1: {
+    textAlign: 'center',
+    fontSize: '13px',
+    fontWeight: '300',
+    a: {
+      textDecoration: 'none',
+    }
+  }
+}));
