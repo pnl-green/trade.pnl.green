8c0276bd02e4107cc3d93e271ffc0c2ec6d39927
ehfuzzz <146187954+ehfuzzz@users.noreply.github.com>
2025-11-23 23:04:43 -0500

    Add Safari overrides for gradient removal

--- frontend/utils/isSafari.ts
+++ frontend/utils/isSafari.ts
@@ -1,7 +1,3 @@
 export const isSafari =
   typeof navigator !== 'undefined' &&
   /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-
-export const isIOS =
-  typeof navigator !== 'undefined' &&
-  /iP(ad|hone|od)/i.test(navigator.userAgent);
